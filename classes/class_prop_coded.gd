13d3e6d7a762eeee206c842a6b5cb6b8e3190c51
Gustaaf Milzink <107410648+G-Milzink@users.noreply.github.com>
2025-09-07 17:53:03 +0200

    Add coded prop and improve message handling
    Introduces a new coded prop to the map and updates the Prop_Coded class to support separate initial and repeat messages, as well as first interaction logic. Also sets autowrap_mode for dev_prop, dev_prop_coded, and dev_prop_flavored scenes to improve text display.

--- classes/class_prop_coded.gd
+++ classes/class_prop_coded.gd
@@ -4,7 +4,12 @@ class_name Prop_Coded
 #===============================================================================
 
 @export var canReceiveCode: bool = true
-@export_multiline var intialMessage: Array[String] = [
+@export_multiline var initialMessage: Array[String] = [
+	"Use this to wrap the ",
+	"*code*",
+	" in a proper sentence"
+]
+@export_multiline var repeatMessage: Array[String] = [
 	"Use this to wrap the ",
 	"*code*",
 	" in a proper sentence"
@@ -12,13 +17,20 @@ class_name Prop_Coded
 
 var hasCode: bool = false
 var code: String = ""
+var isFirstInteraction: bool = true
 
 #===============================================================================
 
-func _ready() -> void:
+func  _ready() -> void:
+	getCode()
 	super()
 
-#===============================================================================
+func setMessage():
+	if isFirstInteraction:
+		message = initialMessage[0] + code + initialMessage[2]
+		isFirstInteraction = false
+	else:
+		message = repeatMessage[0] + code + repeatMessage[2]
 
 func getCode() -> String:
 	canReceiveCode = false
